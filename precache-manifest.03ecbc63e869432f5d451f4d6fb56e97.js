8d408fd8a12587ef233cda51782423219feda53d
Okpala Faith <okpalafaith1@gmail.com>
2020-10-15 20:13:35 +0100

    Updates

---
+++ precache-manifest.03ecbc63e869432f5d451f4d6fb56e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a056b11ca4a2294adfb3262549ffdc0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "265315dbb935da019237",
+    "url": "/monsters-rolodex/static/css/main.74fb0ced.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265315dbb935da019237",
+    "url": "/monsters-rolodex/static/js/main.a73246b3.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
